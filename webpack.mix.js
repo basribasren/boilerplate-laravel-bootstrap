cfff2d0d73b5f32f52f2607650fb2fa834161759
basribasren <basri.official.acc@gmail.com>
2019-01-23 11:32:48 -0800

    template

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,23 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+// mix.js('resources/assets/js/app.js', 'public/js')
+// mix.sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.less('app.less');
+mix.copy('bower_components/bootstrap/dist/fonts', 'public/fonts');
+	mix.copy('bower_components/font-awesome/fonts', 'public/fonts');
+	mix.styles([
+    'bower_components/bootstrap/dist/css/bootstrap.css',
+    'bower_components/fontawesome/css/font-awesome.css',
+    'resources/css/sb-admin-2.css',
+    'resources/css/timeline.css'
+], 'public/css/styles.css', './');
+mix.scripts([
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/bootstrap/dist/js/bootstrap.js',
+    'bower_components/Chart.js/Chart.js',
+    'bower_components/metisMenu/dist/metisMenu.js',
+    'resources/js/sb-admin-2.js',
+    'resources/js/frontend.js'
+], 'public/js/frontend.js', './');
